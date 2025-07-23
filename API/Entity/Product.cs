41a37aed3f255c527297b3639c9eb5cd9e0afd88
Atalay Çelik <atalaycelik4@gmail.com>
2025-07-23 19:41:55 +0300

    new file:   API/Controllers/ProductsController.cs 	modified:   API/Entity/Product.cs 	modified:   API/bin/Debug/net9.0/API.dll 	modified:   API/bin/Debug/net9.0/API.exe 	modified:   API/bin/Debug/net9.0/API.pdb 	modified:   API/obj/Debug/net9.0/API.AssemblyInfo.cs 	modified:   API/obj/Debug/net9.0/API.AssemblyInfoInputs.cache 	modified:   API/obj/Debug/net9.0/API.csproj.CoreCompileInputs.cache 	modified:   API/obj/Debug/net9.0/API.csproj.FileListAbsolute.txt 	modified:   API/obj/Debug/net9.0/API.dll 	modified:   API/obj/Debug/net9.0/API.pdb 	new file:   API/obj/Debug/net9.0/API.sourcelink.json 	modified:   API/obj/Debug/net9.0/apphost.exe 	modified:   API/obj/Debug/net9.0/ref/API.dll 	modified:   API/obj/Debug/net9.0/refint/API.dll

--- API/Entity/Product.cs
+++ API/Entity/Product.cs
@@ -8,6 +8,7 @@ public class Product
 
     [Required]
     public string? Name { get; set; }
+    
     public string? Description { get; set; }
     public decimal? Price { get; set; }
     public bool IsActive { get; set; }
--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eaf31eb5dd554cec16ef4ffaa8a5abc2b195efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
